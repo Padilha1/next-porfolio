d8eeaba9326b846dc3d1b98c3e7679ff748ce665
Padilha1 <padilha.matheus@hotmail.com>
2022-10-23 20:55:07 -0300

    NLW link added

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -27,7 +27,7 @@ export default function Card1 (){
 export const Card2 = () => {
     return (
         <div className='text-center shadow-lg p-10 rounded-xl my-10 lg:text-lg dark:shadow-[#352D39]' >
-            <a href=""><Image src={NLW} width={300} height={150} alt=""/></a>
+            <a href="https://nlw-ignite-esports.vercel.app/"><Image src={NLW} width={300} height={150} alt=""/></a>
             <h3 className='text-xl font-semibold pt-7 pb-2 border-b-4 border-purple-700'>NLW eSports web application</h3>
             <p className='py-2 max-w-5xl'>Created a web application where you create/post an AD to find someone to play together.
             You choose the game, pick the day you play and after you can see the ADs created per Game.</p>
